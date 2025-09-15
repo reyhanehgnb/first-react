736a21eb69421f6a01e55050fc9241b17acc5d14
reyhanegh2007 <reyhanehghanbari793@gmail.com>
2025-09-15 17:00:00 +0330

    First part responsive added

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,33 +3,33 @@ import pic1 from "../images/bg-photo.webp";
 const MainPage = () => {
   return (
     <div>
-      <div className="relative w-auto h-screen">
+      <div className="relative w-auto h-60 sm:h-80 md:h-120 lg:h-164.5">
         <img
           src={pic1}
           alt="Background"
           className="absolute inset-0 w-screen h-full object-cover "
         />
         <div className="absolute inset-0 bg-black opacity-60"></div>
-        <div className="relative z-10 flex flex-row justify-around p-10 gap-3  text-[5px] sm:text-[10px] lg:text-[18px]">
+        <div className="relative z-10 flex flex-row justify-around p-3 sm:p-4 md:p-6 lg:p-10  gap-0.5 sm:gap-1 md:gap-2 lg:gap-3  text-[8px] sm:text-[13px] md:text-[13px] lg:text-[18px]">
           <logo className="text-white px-6">reyhaneh</logo>
-          <ul className=" flex flex-row items-center space-x-9">
-            <li className=" text-gray-50 hover:text-gray-500 cursor-pointer transition-colors duration-300">
+          <ul className=" flex flex-row items-center  space-x-3 sm:space-x-7 md:space-x-8 lg:space-x-9">
+            <li className=" text-gray-50 hover:text-[var(--lightpurple)] cursor-pointer transition-colors duration-300">
               Home
             </li>
-            <li className=" text-gray-50 hover:text-gray-500 cursor-pointer transition-colors duration-300">
+            <li className=" text-gray-50 hover:text-[var(--lightpurple)] cursor-pointer transition-colors duration-300">
               Projects
             </li>
-            <li className=" text-gray-50 hover:text-gray-500 cursor-pointer transition-colors duration-300">
+            <li className=" text-gray-50 hover:text-[var(--lightpurple)] cursor-pointer transition-colors duration-300">
               Skills
             </li>
-            <li className=" text-gray-50 hover:text-gray-500 cursor-pointer transition-colors duration-300">
+            <li className=" text-gray-50 hover:text-[var(--lightpurple)] cursor-pointer transition-colors duration-300">
               About Me
             </li>
-            <li className=" text-gray-50 hover:text-gray-500 cursor-pointer transition-colors duration-300">
+            <li className=" text-gray-50 hover:text-[var(--lightpurple)] cursor-pointer transition-colors duration-300">
               Contact Me
             </li>
           </ul>
-          <button className="bg-[var(--lightgray)] px-5.5 py-1 rounded-2xl hover:bg-[var(--darkblue)] hover:text-gray-100 transition-colors duration-300">
+          <button className="bg-[var(--lightgray)] px-1.5 py-0.5 sm:px-2.5 sm:py-1 md:px-3.5 md:py-1 lg:px-5.5 lg:py-1 rounded-2xl hover:bg-[var(--lightpurple)] hover:text-gray-100 transition-colors duration-300">
             Sign In
           </button>
         </div>
